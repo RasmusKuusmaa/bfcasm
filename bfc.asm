b131a1dc8253a5971cb867a3b3236c7a3b8c0541
77f315 <77f315@gmail.com>
2024-11-13 22:53:51 +0200

    going through the code and exiting when finished

--- bfc.asm
+++ bfc.asm
@@ -1,9 +1,18 @@
 section .bss 
     memory resb 30000  
 section .data
-    bf_code db 0
+    bf_code db '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.'
 
 section .text global _start
 _start:
     mov rsi, memory
     mov rbx, bf_code
+
+next_instruction:
+    mov al, [rbx]
+    inc rbx
+
+    cmp al,0
+    je end_program
+    
+
